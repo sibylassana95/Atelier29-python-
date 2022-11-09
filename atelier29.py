2d91f7ea79cd25c6089fe9c05fbbe58b393ed5f6
Lassana siby <103085452+sibylassana95@users.noreply.github.com>
2022-11-09 01:27:19 +0000

    Add files via upload

---
+++ atelier29.py
@@ -0,0 +1,11 @@
+# Ecrire un programme en langage Python qui demande à l’utilisateur 
+# de saisir son âge et de lui afficher le message « vous êtes Majeur ! »
+#  si l’âge tapé est supérieur ou égale à 18 et le message « vous êtes mineur ! »
+#  si l’âge tapé est inférieur à 18
+
+# Demander à l'utilisateur de taper son âge 
+age = int(input("Tapez votre age : "))
+if age > 18:
+    print("Vous êtes majeur !")
+else:
+    print("Vous êtes mineur !")
